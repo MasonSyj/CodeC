94bbde149d1af138d3c65a96ab908156543f0314
MasonShi <shiyujiesx@163.com>
2022-10-17 17:54:45 +0100

    maybe finish crossword

--- Crossword/crossword.c
+++ Crossword/crossword.c
@@ -1,56 +1,96 @@
 #include "crossword.h"
 
+void print(const crossword* c){
+   for (int j = 0; j < c->sz; j++){
+      for (int i = 0; i < c->sz; i++){
+         printf("%-2c", c->arr[j][i]);
+      }
+      printf("\n");
+   }
+   printf("-------------------------\n");
+}
+
 void test(void){
 
 }
+
 
 /*
    Convert a size (sz) & string (ip) into a crossword*
    White squares are ' ' or '.', black squares 'X' or '*'
 */
 bool str2crossword(int sz, char* ip, crossword* cw){
-   if (sz * sz != (signed) strlen(ip) || sz <= 0 || ip == NULL || cw == NULL){
+   if (sz <= 0 || ip == NULL || cw == NULL || sz * sz != (signed) strlen(ip)){
       return false;
    }
    
-   
    for (int j = 0; j < sz; j++){
       for (int i = 0; i < sz; i++){
-         cw->arr[j][i] = *(ip++);
+         cw->arr[j][i] = *ip;
+         if(cw->arr[j][i] == ' '){
+            cw->arr[j][i] = '.';
+         }
+         if(cw->arr[j][i] == '*'){
+            cw->arr[j][i] = 'X';
+         }
+         ip++;
       }
    }
+   cw->sz = sz;
+   print(cw);
    return true;
 }
 
 // Convert crossword to string of across & down numbers
 void getcluestring(const crossword* c, char* str){
-   strcpy(str, "A-");
+   strncpy(str, "A", BIGSTR);
    char b[BIGSTR];
-   strcpy(b, "|B-");
+   strncpy(b, "|D", BIGSTR);
    int bi = 2;
+   int ai = 1;
    int cnt = 1;
    for (int j = 0; j < c->sz; j++){
       for (int i = 0; i < c->sz; i++){
-         if (c->arr[j][i] == 'X' || c->arr[j][i] == '*'){
+         if (c->arr[j][i] == 'X'){
             continue;
-         }else if (c->arr[j][i] == ' ' || c->arr[j][i] == '.'){
-            if ((j == 0 || c->arr[j-1][i] == 'X' || c->arr[j-1][i] == '*') && (i == 0 || c->arr[j][i-1] == 'X' || c->arr[j][i-1] == '*')){
-               *(str++) = cnt + '0';
-               *(str++) = '-';
-               b[bi++] = cnt + '0';
+         }else if (c->arr[j][i] == '.'){
+            if (((j == 0 || c->arr[j-1][i] == 'X' ) && j < c->sz - 1 && c->arr[j+1][i] == '.') && ((i == 0 || c->arr[j][i-1] == 'X' ) && i < c->sz - 1 && c->arr[j][i+1] == '.')){
+               str[ai++] = '-';
+               if (cnt >= 10){
+                  str[ai++] = cnt / 10 + '0';
+                  str[ai++] = cnt % 10 + '0';
+               }else{
+                  str[ai++] = cnt + '0';
+               }
                b[bi++] = '-';
+               if (cnt >= 10){
+                  b[bi++] = cnt / 10 + '0';
+                  b[bi++] = cnt % 10 + '0';
+               }else{
+                  b[bi++] = cnt + '0';
+               }
                cnt++;
             }
          
-            else if (j == 0 || c->arr[j-1][i] == 'X' || c->arr[j-1][i] == '*'){
-               *(str++) = cnt + '0';
-               *(str++) = '-';
+            else if ((j == 0 || c->arr[j-1][i] == 'X' ) && j < c->sz - 1 && c->arr[j+1][i] == '.'){
+               b[bi++] = '-';
+               if (cnt >= 10){
+                  b[bi++] = cnt / 10 + '0';
+                  b[bi++] = cnt % 10 + '0';
+               }else{
+                  b[bi++] = cnt + '0';
+               }
                cnt++;
             }
             
-            else if (i == 0 || c->arr[j][i-1] == 'X' || c->arr[j][i-1] == '*'){
-            	b[bi++] = cnt + '0';
-               b[bi++] = '-';
+            else if ((i == 0 || c->arr[j][i-1] == 'X' ) && i < c->sz - 1 && c->arr[j][i+1] == '.'){
+               str[ai++] = '-';
+               if (cnt >= 10){
+                  str[ai++] = cnt / 10 + '0';
+                  str[ai++] = cnt % 10 + '0';
+               }else{
+                  str[ai++] = cnt + '0';
+               }
                cnt++;
             }
          }
@@ -58,22 +98,61 @@ void getcluestring(const crossword* c, char* str){
    }
    b[bi] = '\0';
    strcat(str, b);
+   puts(str);
 }
 
 
 // Get percentage of empty squares that are shared between two clues
 int getchecked(crossword c){
    int cnt = 0;
+   int check = 0;
+   int cnti = 0;
+   
+   for (int j = 1; j < c.sz; j++){
+      for (int i = 1; i < c.sz; i++){
+         if (c.arr[j][i] == ' ' || c.arr[j][i] == '.'){
+            if (c.arr[j][i-1] == '.' && c.arr[j-1][i] == '.'){
+               cnti++;
+            }
+         }
+      }
+   } 
+      
    for (int j = 0; j < c.sz; j++){
       for (int i = 0; i < c.sz; i++){
-         if (c.arr[j][i] == 'X' || c.arr[j][i] == '*'){
+         if (c.arr[j][i] == 'X'){
             continue;
-         }else if (c.arr[j][i] == ' ' || c.arr[j][i] == '.'){
-            if ((j == 0 || c.arr[j-1][i] == 'X' || c.arr[j-1][i] == '*') && (i == 0 || c.arr[j][i-1] == 'X' || c.arr[j][i-1] == '*')){
-               cnt++;
+         }else if (c.arr[j][i] == '.'){
+            if ((j == 0 || c.arr[j-1][i] == 'X')
+               && (i == 0 || c.arr[j][i-1] == 'X')){
+                  check++;
+               }
+            
+            else if ((j == 0 || c.arr[j-1][i] == 'X' ) && j < c.sz - 1 && c.arr[j+1][i] == '.'){
+               check++;
+            }
+            
+            else if ((i == 0 || c.arr[j][i-1] == 'X' ) && i < c.sz - 1 && c.arr[j][i+1] == '.'){
+               check++;
             }
          }
       }
    }
-   return cnt;
+   
+   for (int j = 0; j < c.sz; j++){
+      for (int i = 0; i < c.sz; i++){
+         if (c.arr[j][i] == '.'){
+            cnt++;
+         }
+      }
+   }
+   printf("cnt : %d, cntb: %d, cnti: %d\n", cnt, check, cnti);
+   int x = (double) (check+cnti) / (double) cnt * 100 + 0.5;
+   printf("%d\n",x);
+   return x;
 }
+
+//if (c.arr[j][i] == 'X' || c.arr[j][i] == '*'){
+// continue;
+//}else if (c.arr[j][i] == ' ' || c.arr[j][i] == '.'){
+// if ((j == 0 || c.arr[j-1][i] == 'X' || c.arr[j-1][i] == '*') && (i == 0 || c.arr[j][i-1] == 'X' || c.arr[j][i-1] == '*')){
---
+++ Crossword/x.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char a[100] = (char)(10); 
+}

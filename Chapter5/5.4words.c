04896ec02414bb8814f1a0ae9f2a6e5575c25384
MasonShi <shiyujiesx@163.com>
2022-10-19 00:03:45 +0100

    stuck at moving all the array index

--- Chapter5/5.4words.c
+++ Chapter5/5.4words.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ROW 10
-#define COL 20
+#define ROW 5000
+#define COL 10
 
 void makeway(char arr[][COL], int kth);
 void insert(char arr[][COL], char temp[]);
-void show(char arr[][COL]);
+void show(char arr[][COL], int cnt);
 
 int main(){
    FILE* fp = fopen("word.txt", "r");
@@ -21,19 +21,22 @@ int main(){
    i = 0;
    char c;
    char temp[COL];
+   int cnt = 0;
    while ((c = fgetc(fp)) != EOF){
       if (c == '\n'){
+         strcpy(arr[cnt], "");
          temp[i] = '\0';
-//       puts(temp);
-         insert(arr, temp);
          i = 0;
+         cnt++;
+         puts(temp);
+         insert(arr, temp);
          strcpy(temp, "");
       }else{
          temp[i++] = c;
       }
    }
-   printf("------------");
-   show(arr);
+   printf("------------\n");
+   show(arr, cnt);
 
    fclose(fp);
    return EXIT_SUCCESS;
@@ -41,24 +44,40 @@ int main(){
 
 void insert(char arr[][COL], char temp[]){
    int j = 0;
-   while (j++ < ROW-1){
-      if (strcmp(arr[j], temp) < 0 && strcmp(arr[j+1], temp) > 0){
-         makeway(arr, j+1);
-         strcpy(arr[j], temp);
-         puts(arr[j]);
+   while (j < ROW - 1){
+      if (strcmp(arr[0], "") == 0){
+        strcpy(arr[0], temp);
+      }
+      else if (strcmp(temp, arr[0]) < 0){
+         puts("case1");
+         makeway(arr, 0);
+         strcpy(arr[0], temp);
          return;
       }
+      else if (strcmp(temp, arr[j]) > 0 && strcmp(temp, arr[j+1]) < 0){
+        puts("case2");
+         makeway(arr, j+1);
+         strcpy(arr[j+1], temp);
+         return;
+      }else if (strcmp(temp, arr[j]) > 0 && arr[j+1][0] == '\0'){
+         puts("case3");
+         strcpy(arr[j+1], temp);
+         return;
+      }
+      j++;
    }
 }
 
 void makeway(char arr[][COL], int kth){
-   for (int j = ROW - 2; j >= kth; j--){
-      strcpy(arr[j+1], arr[j]);
+   int j = ROW - 2;
+   while (j >= kth){
+     strcpy(arr[j+1], arr[j]);
+     j--;
    }
 }
 
-void show(char arr[][COL]){
-   for (int j = 0; j < ROW; j++){
+void show(char arr[][COL], int cnt){
+   for (int j = 0; j < cnt; j++){
       // if (arr[j][0] == '\0'){
       //    return;
       // }

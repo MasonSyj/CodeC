e3c7313cdc8da505bd980e543b746092f01ee8f6
MasonShi <shiyujiesx@163.com>
2022-10-24 15:14:39 +0100

    open up a new file pointer when an anagram is found in f(recur)

--- Chapter5/5.1.2anagram.c
+++ Chapter5/5.1.2anagram.c
@@ -8,46 +8,41 @@ void swap(char* a, char* b);
 void permuatecnt(char a[], int n, FILE* fp);
 
 int main(void){
-   FILE* fp = fopen("34words.txt", "r");
-   FILE* fp2 = fopen("34words.txt", "r");
+   FILE* fp = fopen("words.txt", "r");
+
    if (!fp){
       fprintf(stderr, "txt failed to access");
       exit(EXIT_FAILURE);
    }
    char str[SIZE];
    while (fgets(str, SIZE, fp) != NULL){
-/**      
-      if (fgets(str, SIZE, fp) == NULL){
-         fprintf(stderr, "cannot access line.");
-         exit(EXIT_FAILURE);
-      }
-*/      
-      fp2 = fp;
       int len = strlen(str);
       str[len - 1] = '\0';
-      permuatecnt(str, len - 2, fp2);
-      printf("next one::\n");
+      printf("\n%s: ", str);
+      permuatecnt(str, len - 2, fp);
    }
-   
-   
+
+
    fclose(fp);
 }
 
 void permuatecnt(char a[], int n, FILE* fp){
    if (n == 0){
-      puts(a);
-		char str[SIZE];
-		while (fgets(str, SIZE, fp) != NULL){
-         int len = strlen(str);
-         str[len - 1] = '\0';
-         if (strcmp(a, str) == 0){
-            printf("%s   ", a);
-         }
-		}
-		printf("\n");
+		 char str[SIZE];
+     FILE* fp2 = fopen("words.txt", "r");
+		 while (fgets(str, SIZE, fp2) != NULL){
+        int len = strlen(str);
+        str[len - 1] = '\0';
+        if (strcmp(a, str) == 0){
+           printf("%s ", a);
+           fprintf(fp2, " ");
+        }
+
+		 }
+     fclose(fp2);
 		return;
 	}
-	
+
 	for (int i = n; i >= 0; i--){
 		swap(&a[i], &a[n]);
 		permuatecnt(a, n - 1, fp);

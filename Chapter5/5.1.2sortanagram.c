e9094f20f3e24bf781814e2b3e146d7754c21505
MasonShi <shiyujiesx@163.com>
2022-10-24 22:57:08 +0100

    issue related to output

--- Chapter5/5.1.2sortanagram.c
+++ Chapter5/5.1.2sortanagram.c
@@ -23,6 +23,7 @@ bool isana(char* a, char* b);
 int count(char* str, int n);
 void insert(node* head, char* temp);
 void show(node* head);
+void anaprint(anode* a);
 
 int main(void){
    FILE* fp = fopen("34words.txt", "r");
@@ -44,53 +45,62 @@ int main(void){
       temp[length - 1] = '\0';
       insert(head, temp);
    }
+   show(head);
    analist(head);
 
 }
 
 void analist(node* head){
    head = head->next;
-   int cnt = 0;
+   anode* ahead;
    while(head){
+      int cnt = 0;
       bool exist = 0;
       struct node* iter = head->next;
       while(iter && iter->invoice == head->invoice){
+         anode* a = (anode*)malloc(sizeof(struct anode));
+         ahead = a;
+         a->next = NULL;
+//       ahead.next = a;
          if (strlen(head->word) == strlen(iter->word) && isana(head->word, iter->word)){
-           if (exist == 0){
-             anode head;
-             head->next = a;
-             anode* a = (anode*)malloc(sizeof(struct(anode)));
-             strcpy(a->word, head->word);
-             a->next = (anode*)malloc(sizeof(struct(anode)));
-             a = a->next;
-             strcpy(a->word, iter->word);
-             a->next = NULL;
-             exist = 1;
-             cnt += 2;
+            if (exist == 0){
+               strcpy(a->word, head->word);
+//             puts(a->word);
+               a->next = (anode*)malloc(sizeof(struct anode));
+               a = a->next;
+               strcpy(a->word, iter->word);
+               a->next = NULL;
+               exist = 1;
+               cnt += 2;
+//             puts(a->word);
            }else{
-             a->next = (anode*)malloc(sizeof(struct(anode)));
+             a->next = (anode*)malloc(sizeof(struct anode));
              a = a->next;
              strcpy(a->word, iter->word);
+//           puts(a->word);
              a->next = NULL;
              cnt++;
            }
-            // if (exist == false){
-            //    printf("%s ", head->word);
-            //    printf("%s ", iter->word);
-            //    exist = 1;
-            // }else{
-            //    printf("%s ", iter->word);
-            // }
+
          }
          iter = iter->next;
+         if (iter && iter->invoice != head->invoice && exist){
+              printf("%d ", cnt);
+              anaprint(ahead);
+         }
       }
-      if (exist == true){
-         a = head->next;
-         printf("%d ", cnt);
-         printf("%s ", a->word);
-      }
+
       head = head->next;
    }
+}
+
+void anaprint(anode* a){
+
+   while (a){
+      printf("%s ", a->word);
+      a = a->next;
+   }
+   printf("\n--------------------\n");
 }
 
 void insert(node* head, char temp[]){
@@ -127,10 +137,8 @@ void insert(node* head, char temp[]){
 int count(char* str, int n){
    int count = 0;
    int i = 0;
-   while(i <= n){
-
-     count += *str - 'a' + 1;
-     i++;
+   while(i < n){
+     count += str[i++] - 'a' + 1;
    }
 // printf("%d\n", count);
    return count;
@@ -162,3 +170,5 @@ bool isana(char* a, char* b){
    }
    return true;
 }
+
+

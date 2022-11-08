999fbfb322d08e05be502ed09ed800613b1aeb20
MasonShi <shiyujiesx@163.com>
2022-11-08 19:49:01 +0000

    slight improvment on siersqure

--- Chapter7recursion/sierpinskisquare2.c
+++ Chapter7recursion/sierpinskisquare2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-#define LINE 81
+#define LINE 40
 
-void clear(char sier[][LINE+1]);
 void draw(char sier[][LINE+1], int y, int x, int length);
-void sierpinski(char sier[][LINE+1], int y, int x, int mark);
+void sierpinski(char sier[][LINE+1], int y, int x, int len);
 void print(char sier[][LINE+1]);
 
 int main(void){
@@ -17,7 +16,7 @@ int main(void){
    }
 //   print(sier);   
 // int mark = LINE / 3;
-   sierpinski(sier, 0, 0, LINE / 2);
+   sierpinski(sier, 0, 0, LINE);
 /*   for (int j = mark; j < mark + mark; j++){
       for (int i = mark; i < mark + mark; i++){
          sier[j][i] = '0';
@@ -27,31 +26,31 @@ int main(void){
    print(sier);
 }
 
-void sierpinski(char sier[][LINE+1], int y, int x, int mark){
-   if (mark != LINE / 2){
-      draw(sier, y, x, mark);  
-      
-   }
+void sierpinski(char sier[][LINE+1], int y, int x, int len){
+   draw(sier, y, x, len / 4);
+   draw(sier, y, x + len * 3 / 4, len / 4);
    
    print(sier);
    
-   if (mark == 1 || mark == 0){
+   if (len == 3 || len == 4){
       return;
    }
    
    int y1 = y;
-   int x1 = x;
+   int y2 = len / 2 + y;
+   int y3 = len / 2 + y;
    
-   int y2 = y;
-   int x2 = x + mark * 3 / 4;
+   int x1 = x + len / 4;
+   int x2 = x;
+   int x3 = x + len / 2;
    
-   int div = mark / 4;
+   int nextlen = len / 2;
    
 // clear(sier);
    
-   sierpinski(sier, y1, x1, div);
-
-   sierpinski(sier, y2, x2, div);
+   sierpinski(sier, y1, x1, nextlen);
+   sierpinski(sier, y2, x2, nextlen);
+   sierpinski(sier, y3, x3, nextlen);
 
    
    
@@ -59,12 +58,12 @@ void sierpinski(char sier[][LINE+1], int y, int x, int mark){
 
 void draw(char sier[][LINE+1], int y, int x, int length){
 // printf("y = %d, x = %d, length = %d\n", y, x, length);
-   for (int j = y; j < y + length * 2; j++){
-      for (int i = x; i < x + length; i++){
+   for (int j = y; j < y + length * 2 + 1; j++){
+      for (int i = x; i < x + length + 1; i++){
          sier[j][i] = '.';
       }
    }
-}
+} 
 
 void print(char sier[][LINE+1]){
    for (int j = 0; j < LINE; j++){
@@ -76,10 +75,3 @@ void print(char sier[][LINE+1]){
    printf("------------------------------------------------------------------------\n");
 }
 
-void clear(char sier[][LINE+1]){
-   for (int j = 0; j < LINE; j++){
-      for (int i = 0; i < LINE; i++){
-         sier[j][i] = '.';
-      }
-   }
-}

9f5adf964944f3617ba46a30da04f125404d675d
MasonSyj <shiyujiesx@163.com>
2022-11-09 15:59:19 +0000

    using simple struct to contain prime num

--- Chapter7recursion/primefactor.c
+++ Chapter7recursion/primefactor.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void primef(int n);
+void primef(int* t, int n);
 
 int main(void){
-   int x = 210;
-   printf("%d: ", x);
-   primef(x);
-   printf("\n%d: ", 117);
-   primef(117);
-   printf("\n%d: ", 768);
-   primef(768);
+   int x = 31;
+   int* t = (int*)calloc(x, sizeof(t));
+   printf("%d = 1", x);
+   primef(t, x);
+   
+   for (int i = 0; i < x; i++){
+      if (t[i] > 1){
+         printf(" x %d^%d", i+1, t[i]);
+      }else if (t[i] == 1){
+         printf(" x %d", i+1);
+      }
+   }
+   
 }
 
-void primef(int n){
+void primef(int* t, int n){
    if (n == 1){
       return;
    }
    
    for (int i = 2; i <= n; i++){
       if (n % i == 0){
-         primef(n/i);
-         printf("%d   ", i);
+         primef(t, n / i);
+         t[i - 1]++;
          return;
       }
    }

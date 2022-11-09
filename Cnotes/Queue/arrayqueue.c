0dde2c28f9ae827c5464d21876bc052d3c7b8353
MasonSyj <shiyujiesx@163.com>
2022-11-09 16:48:51 +0000

    try queue in fixed array

--- Cnotes/Queue/arrayqueue.c
+++ Cnotes/Queue/arrayqueue.c
@@ -4,69 +4,110 @@
 #include <assert.h>
 
 #define SIZE 20
-#define default -1
+#define DEFAULT -1
 
 typedef int datatype;
 
-
 typedef struct queue{
    datatype data[SIZE];
-   int rear;
+   int end;
    int front;
 }queue;
 
-queue* init(void);
-bool isfull(queue* q);
-bool isempty(queue* q);
-void enqueue(queue* q, datatype data);
-datatype dequeue(queue* q);
+queue* queue_init(void);
+void queue_enqueue(queue* q, datatype data);
+datatype queue_dequeue(queue* q);
+int queue_size(queue* q);
+bool queue_free(queue* q);
+void _inc(int* p);
 
 int main(void){
-   queue* q = init();
-   assert(!isfull(q));
-   assert(isempty(q));
-   enqueue(q, 15);
-   assert(!isfull(q));
-   printf("q->rear:%d\n", q->rear);
-   printf("q->data:%d\n", q->data[q->front+1]);
-   assert(!isempty(q));
-   assert(dequeue(q) == 15);
+   queue* q = queue_init();
+   assert(queue_size(q) == 0);
+   queue_enqueue(q, 15);
+   assert(queue_size(q) == 1);
+   queue_enqueue(q, 25);
+   assert(queue_size(q) == 2);
+   queue_enqueue(q, 35);
+   assert(queue_size(q) == 3);
+   assert(queue_dequeue(q) == 15);
+   assert(queue_size(q) == 2);
+   assert(queue_dequeue(q) == 25);
+   assert(queue_size(q) == 1);
+   assert(queue_dequeue(q) == 35);
+   assert(queue_size(q) == 0);
+   
+ /* assertion below should test the _inc function**/
+   for (int i = 0; i < SIZE; i++){
+      queue_enqueue(q, i);
+   }
+   for (int i = 0; i < SIZE / 2; i++){
+      assert(queue_dequeue(q) == i);
+   }
+   
+   for (int i = 0; i < SIZE / 2; i++){
+      queue_enqueue(q, i * 2);
+   }
+   
+   for (int i = 0; i < SIZE / 2; i++){
+      assert(queue_dequeue(q) == i + SIZE / 2);
+   }
+   
+   for (int i = 0; i < SIZE / 2; i++){
+      assert(queue_dequeue(q) == i * 2);
+   }
+   
+   
 }
 
-queue* init(void){
+queue* queue_init(void){
    queue* q = (queue*)malloc(sizeof(queue));
-   q->front = -1;
-   q->rear = -1;
+   q->front = 0;
+   q->end = 0;
    for (int i = 0; i < SIZE; i++){
-      q->data[i] = default;
+      q->data[i] = DEFAULT;
    }
    return q;
 }
 
-bool isfull(queue* q){
-   return q->rear == q->front && q->rear != -1;
-}
-bool isempty(queue* q){
-   return q->front == -1 && q->rear == -1;
-
-   return q->rear == q->front && q->data[q->front] == -1;
-}
-
-void enqueue(queue* q, datatype data){
-   if (isfull(q)){
+void queue_enqueue(queue* q, datatype data){
+   if (q->end == q->front && q->data[0] != DEFAULT){
       printf("full-> unable to add->\n");
       return;
    }else{
-      q->data[++q->rear] = data;
+      q->data[q->end] = data;
+      _inc(&q->end);
       return;
    }
 }
 
-datatype dequeue(queue* q){
-   if (isempty(q)){
+datatype queue_dequeue(queue* q){
+   if (q->end == q->front && q->data[0] == DEFAULT){
       printf("empty unable to remove return default value\n");
-      return default;
+      return DEFAULT;
    }else{
-      return q->data[++q->front];
+      datatype data = q->data[q->front];
+      q->data[q->front] = DEFAULT;
+      _inc(&q->front);
+      return data;
    }
+}
+
+int queue_size(queue* q){
+   if(!q){
+      return -1;
+   }else if (q->end >= q->front){
+      return q->end - q->front;
+   }else{
+      return q->front - q->end + SIZE;
+   }
+}
+
+bool queue_free(queue* q){
+   free(q);
+   return true;
+}
+
+void _inc(int* p){
+   *p = (*p + 1) % SIZE;
 }

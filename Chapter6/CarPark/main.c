ab1ceb9c98e8215ecd8245feeed43323895c895a
MasonSyj <shiyujiesx@163.com>
2022-11-16 16:39:17 +0000

    remove all stupid alloc

--- Chapter6/CarPark/main.c
+++ Chapter6/CarPark/main.c
@@ -11,6 +11,7 @@
 #define CAPACITY 20
 #define CARSIZE 26
 #define A 'A'
+#define STRSIZE 100
 
 typedef struct park{
    char a[CAPACITY][CAPACITY+1];
@@ -21,17 +22,15 @@ typedef struct list{
    park p[N];
    int rowsize;
    int colsize;
-   int current;
+   int end;
 }list;
 
-//question 1, can i calulate park's row and col by function instead of store information into park;
 //question 2, what does const char*, the const mean?
 //question 3, test in the extension.c?
-//question 4, alloc issue?
 
 
 // read from a file initiate the list and the first carpark
-list* parkinit(void);
+void parkinit(list* state);
 // main control function to process one carpark's all possible moves 
 void movectrl(list* l, int index);
 // based on the car, find out the its [y][x] in the 2D array
@@ -49,7 +48,7 @@ park parkcopy(park p, int index);
 // count the number of cars in a carpark
 int carnum(list* l, int index); 
 // get the list of cars in a carpark
-char* carlist(list* l, int index); 
+void carlist(list* l, int index, char* list);
 // check if two carparks are same, true if same
 bool samepark(park p1, park p2, list* l); 
 // show the park
@@ -79,7 +78,7 @@ char leftsquare(int y, int x, list* l, int index);
 // see what the right square is, if not bollard, prepare for move up
 char rightsquare(int y, int x, list* l, int index); 
 // convert a park into a 1D array for testing
-char* tostring(park p, int row, int col);
+void tostring(park p, int row, int col, char* str);
 // check if the park is in right shape which car is connected and length at least 2
 bool rightshape(list* l, int index);
 // given a car, no other place should apper except its head and tail and between.
@@ -91,9 +90,9 @@ bool iscar(park p, int j, int i);
 // check if the park is empty, if empty, then means solved
 bool empty(list* l, int index); //
 // record the index of the correct moves into a 1D array
-int* printlist(list* l, int index);
+void printlist(list* l, int index, int* listint, int len);
 // based the array recording parent index, show all carparks
-void showlist(list* l, int index);//
+void showlist(list* l, int index, int* listint);
 // based on the car in a carpark, get its head and tail(two [y][x] naturally)
 void carcoord(int* y1, int* y2, int* x1, int* x2, park p, char car);
 // return one park's rowsize
@@ -108,66 +107,68 @@ no cars full closed non consec
 */
    
 int main(void){
-   test1();
-   test2();
-   
-   list* l = parkinit();
-   bool show = true;
+//  test1();
+//  test2();
+   static list state;
+   parkinit(&state);
+   bool isshow = true;
    int i = 0;
-   while(i <= l->current){
-      if (solvemovescnt(l, i) > 0){
-         if (show == true){
-           showlist(l, i);
+   int listshow[STRSIZE] = {0};
+   while(i <= state.end){
+      if (solvemovescnt(&state, i) > 0){
+         printf("%d\n", solvemovescnt(&state, i));
+         if (isshow == true){
+           printlist(&state, i, listshow, solvemovescnt(&state, i) + 1);
+           showlist(&state, i, listshow);
          }
-         printf("%d moves", solvemovescnt(l, i));
+         printf("i: %d  %d moves", i, solvemovescnt(&state, i));
          exit(EXIT_SUCCESS);
       }
    
-      if (empty(l, i) == true){
+      if (empty(&state, i) == true){
          fprintf(stderr, "No more new carparks, failed to solve.");
          exit(EXIT_FAILURE);
       }
    
-      movectrl(l, i);
+      movectrl(&state, i);
       i++;
    }
    
    return 0;
 }
    
-list* parkinit(void){
-   FILE* fp = fopen("11x9_10c_26t.prk", "r");
+void parkinit(list* state){
+   FILE* fp = fopen("10x7_8c_16t.prk", "r");
    int row, col;
    char x;
    assert(fscanf(fp, "%d%c%d", &row, &x, &col) == 3);
    
    char temp[CAPACITY];
    fgets(temp, CAPACITY, fp);
-   list* l = (list*)calloc(1, sizeof(list));
-   l->rowsize = row;
-   l->colsize = col;
+   state->rowsize = row;
+   state->colsize = col;
    
    for (int j = 0; j < row; j++){
       fgets(temp, CAPACITY, fp);
       temp[col] = '\0';
-      strncpy(l->p[0].a[j], temp, col);
+      strncpy(state->p[0].a[j], temp, col);
    }
-   l->current = 0;
+   state->end = 0;
    
-   l->p[0].parentindex = 0;
+   state->p[0].parentindex = 0;
    
    fclose(fp);
-   return l;
+   return;
 }
    
 void movectrl(list* l, int index){
-   char* list = carlist(l, index);
+   char list[CARSIZE];
+   carlist(l, index, list);
    unsigned int i = 0;
    while (i < strlen(list)){
       carposition(l, index, list[i]);
       i++;
    }
-   free(list);
 }
 
 void carcoord(int* y1, int* y2, int* x1, int* x2, park p, char car){
@@ -260,12 +261,12 @@ park parkcopy(park p, int index){
 }
    
 void add2list(list* l, park new){
-   for (int i = 0; i <= l->current; i++){
+   for (int i = 0; i <= l->end; i++){
       if (samepark(l->p[i], new, l)){
          return;
       }
    }
-   l->p[++l->current] = new;
+   l->p[++l->end] = new;
    return;
 }
    
@@ -280,7 +281,7 @@ bool samepark(park p1, park p2, list* l){
    return true;
 }
    
-char* carlist(list* l, int index){
+void carlist(list* l, int index, char* list){
    int cnt = 0;
    bool temp[CARSIZE] = {0};
    char car[CARSIZE];
@@ -291,11 +292,8 @@ char* carlist(list* l, int index){
             car[cnt++] = l->p[index].a[j][i];
          }
        }
-   }
-   char* list = (char*)calloc(cnt + 1, sizeof(char));	
+   }	
    strncpy(list, car, cnt);
-   	
-   return list;
 }
 
 void show(park p){
@@ -310,7 +308,9 @@ void show(park p){
 }
    
 int carnum(list* l, int index){
-   return strlen(carlist(l, index));
+   char list[CARSIZE];
+   carlist(l, index, list);
+   return strlen(list);
 }
 
 bool samesign(park p, char car){
@@ -361,7 +361,8 @@ bool noother(park p, char car){
 }
 
 bool rightshape(list* l, int index){
-   char* list = carlist(l, index);
+   char list[STRSIZE];
+   carlist(l, index, list);
    int len = strlen(list);
    int i = 0;
    while (i < len){
@@ -487,33 +488,31 @@ bool iscar(park p, int j, int i){
 }
 
 
-int* printlist(list* l, int index){
-   int len = solvemovescnt(l, index);
-   int* printl = (int*)calloc(++len, sizeof(int));
+void printlist(list* l, int index, int* listint, int len){
    while (index != 0){
-      printl[--len] = index;
+//      printf("index: %d  len: %d\n", index, len);
+      listint[--len] = index;
       index = l->p[index].parentindex;
    }
-   return printl;
+   return;
 }
 
-void showlist(list* l, int index){
-   int* printl = printlist(l, index);
+void showlist(list* l, int index, int* listint){
    for (int i = 0; i < solvemovescnt(l, index) + 1; i++){
-      show(l->p[printl[i]]);
+      show(l->p[listint[i]]);
    }
 }
 
-char* tostring(park p, int row, int col){
-   char* str = (char*)calloc(CAPACITY * CAPACITY, sizeof(char));
+void tostring(park p, int row, int col, char* str){
+   strcpy(str, "");
    char* head = str;
    for (int j = 0; j < row; j++){
       for (int i = 0; i < col; i++){
-         *str++ = p.a[j][i];
+         *head++ = p.a[j][i];
       }
    }
-   *str = '\0';
-   return head;
+   *head = '\0';
+   return;
 }
 
 int rowsize(park p){
@@ -529,59 +528,71 @@ int colsize(park p){
 
 
 void test1(){
-   list* l = (list*)calloc(1, sizeof(list));
-   l->rowsize = 7;
-   l->colsize = 7;
-   l->current = 0;
-
+   static list state;
+   state.rowsize = 7;
+   state.colsize = 7;
+   state.end = 0;
+   char str1[STRSIZE];
+   char str2[STRSIZE];
    
-   strcpy(l->p[0].a[0], "#.#####");
-   strcpy(l->p[0].a[1], ".BBB..#");
-   strcpy(l->p[0].a[2], "#A....#");
-   strcpy(l->p[0].a[3], "#A...C#");
-   strcpy(l->p[0].a[4], "#A...C#");
-   strcpy(l->p[0].a[5], "#..DDC#");
-   strcpy(l->p[0].a[6], "#######");
-   assert(l->current == 0);
-   assert(carnum(l, 0) == 4);
-   assert(strcmp(carlist(l, 0), "BACD") == 0);
+   strcpy(state.p[0].a[0], "#.#####");
+   strcpy(state.p[0].a[1], ".BBB..#");
+   strcpy(state.p[0].a[2], "#A....#");
+   strcpy(state.p[0].a[3], "#A...C#");
+   strcpy(state.p[0].a[4], "#A...C#");
+   strcpy(state.p[0].a[5], "#..DDC#");
+   strcpy(state.p[0].a[6], "#######");
+   assert(state.end == 0);
+   assert(carnum(&state, 0) == 4);
+   carlist(&state, 0, str1);
+   assert(strcmp(str1, "BACD") == 0);
 
-   assert(iscar(l->p[0], 1, 1));
-   assert(iscar(l->p[0], 2, 1));
-   assert(!iscar(l->p[0], 2, 2));
-   assert(!iscar(l->p[0], 3, 3));
+   assert(iscar(state.p[0], 1, 1));
+   assert(iscar(state.p[0], 2, 1));
+   assert(!iscar(state.p[0], 2, 2));
+   assert(!iscar(state.p[0], 3, 3));
 
 
-   assert(upsquare(1, 1, l, 0) == EMPTY);
-   assert(upsquare(3, 5, l, 0) == EMPTY);
-   assert(downsquare(1, 4, l, 0) == EMPTY);
-   assert(downsquare(5, 5, l, 0) == FULL);
-   assert(leftsquare(5, 3, l, 0) == EMPTY);
-   assert(rightsquare(5, 5, l, 0) == FULL);
-   assert(rightsquare(1, 3, l, 0) == EMPTY);
+   assert(upsquare(1, 1, &state, 0) == EMPTY);
+   assert(upsquare(3, 5, &state, 0) == EMPTY);
+   assert(downsquare(1, 4, &state, 0) == EMPTY);
+   assert(downsquare(5, 5, &state, 0) == FULL);
+   assert(leftsquare(5, 3, &state, 0) == EMPTY);
+   assert(rightsquare(5, 5, &state, 0) == FULL);
+   assert(rightsquare(1, 3, &state, 0) == EMPTY);
    
-   char* pstr = tostring(l->p[0], 7, 7);
-   assert(strcmp(pstr, "#.#####.BBB..##A....##A...C##A...C##..DDC########") == 0);
 
-   moveup(3, 5, 5, &l->p[0]); 
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##A...C##A...C##A...C##..DD.########") == 0);
-   moveup(2, 4, 5, &l->p[0]); 
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB.C##A...C##A...C##A....##..DD.########") == 0);
-   movedown(1, 3, 5, &l->p[0]); 
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##A...C##A...C##A...C##..DD.########") == 0);
-   moveleft(3, 4, 5, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##A...C##A...C##A...C##.DD..########") == 0);
-   moveleft(2, 3, 5, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##A...C##A...C##A...C##DD...########") == 0);
-   moveright(1, 2, 5, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##A...C##A...C##A...C##.DD..########") == 0);
-   movedown(2, 4, 1, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##....C##A...C##A...C##ADD..########") == 0);
-   moveleft(1, 3, 1, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####BBB...##....C##A...C##A...C##ADD..########") == 0);
-   moveright(0, 2, 1, &l->p[0]);
-   assert(strcmp(tostring(l->p[0], 7, 7), "#.#####.BBB..##....C##A...C##A...C##ADD..########") == 0);
-   assert(!empty(l, 0));
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A....##A...C##A...C##..DDC########") == 0);
+
+   moveup(3, 5, 5, &state.p[0]); 
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A...C##A...C##A...C##..DD.########") == 0);
+   moveup(2, 4, 5, &state.p[0]); 
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB.C##A...C##A...C##A....##..DD.########") == 0);
+   movedown(1, 3, 5, &state.p[0]); 
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A...C##A...C##A...C##..DD.########") == 0);
+   moveleft(3, 4, 5, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A...C##A...C##A...C##.DD..########") == 0);
+   moveleft(2, 3, 5, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A...C##A...C##A...C##DD...########") == 0);
+   moveright(1, 2, 5, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##A...C##A...C##A...C##.DD..########") == 0);
+   movedown(2, 4, 1, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##....C##A...C##A...C##ADD..########") == 0);
+   moveleft(1, 3, 1, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####BBB...##....C##A...C##A...C##ADD..########") == 0);
+   moveright(0, 2, 1, &state.p[0]);
+   tostring(state.p[0], 7, 7, str1);
+   assert(strcmp(str1, "#.#####.BBB..##....C##A...C##A...C##ADD..########") == 0);
+   assert(!empty(&state, 0));
    
    park closedp;
    strcpy(closedp.a[0], "#######");
@@ -591,9 +602,9 @@ void test1(){
    strcpy(closedp.a[4], "#A...C#");
    strcpy(closedp.a[5], "#..DDC#");
    strcpy(closedp.a[6], "#######");
-   add2list(l, closedp);
-   assert(l->current == 1);
-   assert(carnum(l, 1) == 4);
+   add2list(&state, closedp);
+   assert(state.end == 1);
+   assert(carnum(&state, 1) == 4);
    
    park emptyp;
    strcpy(emptyp.a[0], "#######");
@@ -603,10 +614,10 @@ void test1(){
    strcpy(emptyp.a[4], "#.....#");
    strcpy(emptyp.a[5], "#.....#");
    strcpy(emptyp.a[6], "#######");
-   add2list(l, emptyp);
-   assert(l->current == 2);
-   assert(empty(l, 2));
-   assert(carnum(l, 2) == 0);
+   add2list(&state, emptyp);
+   assert(state.end == 2);
+   assert(empty(&state, 2));
+   assert(carnum(&state, 2) == 0);
 
    park exitp;
 
@@ -617,37 +628,41 @@ void test1(){
    strcpy(exitp.a[4], "#..E.B#");
    strcpy(exitp.a[5], "#..E.B#");
    strcpy(exitp.a[6], "###.#B#");
-   add2list(l, exitp);
-   assert(l->current == 3);
-   assert(!empty(l, 3));
-   assert(carnum(l, 3) == 6);
-   assert(strcmp(carlist(l, 3), "CAFDEB") == 0);
+   add2list(&state, exitp);
+   assert(state.end == 3);
+   assert(!empty(&state, 3));
+   assert(carnum(&state, 3) == 6);
+   carlist(&state, 3, str1);
+   assert(strcmp(str1, "CAFDEB") == 0);
 
-   park copy = parkcopy(l->p[3], 3);
-   assert(strcmp(tostring(copy, 7, 7), tostring(l->p[3], 7, 7)) == 0);
+   park copy = parkcopy(state.p[3], 3);
+   tostring(copy, 7, 7, str1);
+   tostring(state.p[3], 7, 7, str2);
+   assert(strcmp(str1, str2) == 0);
    // add a same one to the list, thcarpositione same one won't add and will be freed.
-   add2list(l, copy);
-   assert(l->current == 3);
+   add2list(&state, copy);
+   assert(state.end == 3);
 
-   assert(reachVerticalboundary(1, l));
-   assert(reachVerticalboundary(5, l));
-   assert(!reachVerticalboundary(3, l));
-   assert(reachHorizontboundary(1, l));
-   assert(!reachHorizontboundary(4, l));
-   assert(!reachHorizontboundary(2, l));
+   assert(reachVerticalboundary(1, &state));
+   assert(reachVerticalboundary(5, &state));
+   assert(!reachVerticalboundary(3, &state));
+   assert(reachHorizontboundary(1, &state));
+   assert(!reachHorizontboundary(4, &state));
+   assert(!reachHorizontboundary(2, &state));
 
+   tostring(exitp, 7, 7, str1);
    carVerticalexit(0, 2, 4, &exitp);
-   assert(strcmp(tostring(exitp, 7, 7), "####.##AAA...##.....#.FFF.DD#..E.B##..E.B####.#B#") == 0);
+   assert(strcmp(str1, "####.##AAA...##.....#.FFF.DD#..E.B##..E.B####.#B#") == 0);
    carHorizontexit(0, 2, 1, &exitp);
-   assert(strcmp(tostring(exitp, 7, 7), "####.##......##.....#.FFF.DD#..E.B##..E.B####.#B#") == 0);
+   assert(strcmp(str1, "####.##......##.....#.FFF.DD#..E.B##..E.B####.#B#") == 0);
    carHorizontexit(5, 6, 3, &exitp);
-   assert(strcmp(tostring(exitp, 7, 7), "####.##......##.....#.FFF...#..E.B##..E.B####.#B#") == 0);
+   assert(strcmp(str1, "####.##......##.....#.FFF...#..E.B##..E.B####.#B#") == 0);
    carVerticalexit(4, 6, 5, &exitp);
-   assert(strcmp(tostring(exitp, 7, 7), "####.##......##.....#.FFF...#..E..##..E..####.#.#") == 0);
+   assert(strcmp(str1, "####.##......##.....#.FFF...#..E..##..E..####.#.#") == 0);
  
-   assert(!samepark(l->p[0], l->p[1], l));
-   assert(!samepark(l->p[1], l->p[2], l));
-   assert(samepark(l->p[1], l->p[1], l));
+   assert(!samepark(state.p[0], state.p[1], &state));
+   assert(!samepark(state.p[1], state.p[2], &state));
+   assert(samepark(state.p[1], state.p[1], &state));
 
    park wrongshape;
    
@@ -659,17 +674,18 @@ void test1(){
    strcpy(wrongshape.a[5], "#..E.B#");
    strcpy(wrongshape.a[6], "###.#B#");
 
-   add2list(l, wrongshape);
-   assert(l->current == 4);
+   add2list(&state, wrongshape);
+   assert(state.end == 4);
 
-   assert(!rightshape(l, 4));
+   assert(!rightshape(&state, 4));
 }
 
 void test2(){
-   list* l = (list*)calloc(1, sizeof(list));
-   l->rowsize = 10;
-   l->colsize = 8;
-   l->current = -1;
+   static list state;
+   state.rowsize = 10;
+   state.colsize = 8;
+   state.end = -1;
+
    park p;
    strcpy(p.a[0], "########");
    strcpy(p.a[1], "#DBBBE..");
@@ -681,33 +697,36 @@ void test2(){
    strcpy(p.a[7], "#D...E.#");
    strcpy(p.a[8], "#......#");
    strcpy(p.a[9], "#.#.#.##");
-   add2list(l, p);
-   assert(l->current == 0);
+   add2list(&state, p);
+   assert(state.end == 0);
    int i = 0;
    int markindex = 0;
    
-   while (i <= l->current){
-      if (solvemovescnt(l, i) > 0){
-         assert(solvemovescnt(l, i) == 13);
+   while (i <= state.end){
+      if (solvemovescnt(&state, i) > 0){
+         assert(solvemovescnt(&state, i) == 13);
          markindex = i;
       }
-      movectrl(l, i);
+      movectrl(&state, i);
       i++;      
    }
-   int* printl = printlist(l, markindex);
-   
-   assert(printl[0] == 0);
-   assert(printl[1] == 1);
-   assert(printl[2] == 4);
-   assert(printl[3] == 10);
-   assert(printl[4] == 20);
-   assert(printl[5] == 36);
-   assert(printl[6] == 60);
 
-   list* l2 = (list*)calloc(1, sizeof(list));
-   l2->rowsize = 7;
-   l2->colsize = 7;
-   l2->current = -1;
+   int listprint[CARSIZE] = {0};
+   printlist(&state, markindex, listprint, 14);
+   
+   assert(listprint[0] == 0);
+   assert(listprint[1] == 1);
+   assert(listprint[2] == 4);
+   assert(listprint[3] == 10);
+   assert(listprint[4] == 20);
+   assert(listprint[5] == 36);
+   assert(listprint[6] == 60);
+
+   static list state2;
+   state2.rowsize = 7;
+   state2.colsize = 7;
+   state2.end = -1;
+
    park p2;
    strcpy(p2.a[0], "#.#####");
    strcpy(p2.a[1], "#......");
@@ -716,20 +735,30 @@ void test2(){
    strcpy(p2.a[4], "#A....#");
    strcpy(p2.a[5], "...DD.#");
    strcpy(p2.a[6], "####.##");
-   add2list(l2, p2);
+   add2list(&state2, p2);
    
    i = 0;
-   moveVertical(2, 4, 1, l2, 0);
-   moveHorizont(3, 4, 5, l2, 0);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######A.....#A....##A....##.....#...DD.#####.##") == 0);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######......#.....##A....##A....#.A.DD.#####.##") == 0);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######......#A....##A....##A....#..DD..#####.##") == 0);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######......#A....##A....##A....#....DD#####.##") == 0);
+   moveVertical(2, 4, 1, &state, 0);
+   moveHorizont(3, 4, 5, &state, 0);
+
+   char str[STRSIZE];
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######A.....#A....##A....##.....#...DD.#####.##") == 0);
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######......#.....##A....##A....#.A.DD.#####.##") == 0);
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######......#A....##A....##A....#..DD..#####.##") == 0);
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######......#A....##A....##A....#....DD#####.##") == 0);
    
-   carposition(l2, 4, 'A');
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######A.....#A....##A....##.....#....DD#####.##") == 0);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######......#.....##A....##A....#.A..DD#####.##") == 0);
+   carposition(&state2, 4, 'A');
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######A.....#A....##A....##.....#....DD#####.##") == 0);
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######......#.....##A....##A....#.A..DD#####.##") == 0);
    
-   movectrl(l2, 5);
-   assert(strcmp(tostring(l2->p[++i], 7, 7), "#.######......#.....##.....##.....#....DD#####.##") == 0);
+   movectrl(&state2, 5);
+   tostring(state.p[++i], 7, 7, str);
+   assert(strcmp(str, "#.######......#.....##.....##.....#....DD#####.##") == 0);
 }
+

359574ed131eb26930fb0eb4921bf9456afb1cc5
MasonSyj <shiyujiesx@163.com>
2022-12-21 11:52:31 +0000

    working on parsing code content

--- NUCLEI/nuclei.c
+++ NUCLEI/nuclei.c
@@ -9,6 +9,7 @@
 #define COL 100
 #define STRSAME(A,B) (strcmp(A,B) == 0)
 #define ERROR(PHARSE) { fprintf(stderr, "Fatal Error %s occurred in %s, line %d\n", PHARSE, __FILE__, __LINE__); exit(EXIT_FAILURE);}
+#define CODESIZE 1000
 
 typedef struct lisp{
    int value;
@@ -27,7 +28,30 @@ typedef struct liststack{
 }stack;
 
 code* this;
+FILE* fp;
 
+void parse(){
+   char* str = (char*)calloc(CODESIZE, sizeof(char));
+   char* temp = (char*)calloc(ROW, sizeof(char));
+   assert(str);
+   assert(temp);
+
+   while (fgets(temp, fp, ROW) == 1){
+      strcat(str, temp);
+   }
+
+   while (*str != '\0'){
+      switch (*str){
+         case '(': this->word[this->currentword++][0] = '('; break;
+         case ')': this->word[this->currentword++][0] = ')'; break;
+         case '\'': literalparse(str); break;
+         case '\"': literalparse(str); break;
+         case ' ':str++; break;
+  
+         default: digitparse(str); break;      
+      }
+   }
+}
 void Prog(void);
 void sinstru(void);
 void instru(void);
@@ -48,11 +72,11 @@ bool print(void);
 bool islist();
 bool isvar();
 bool isliteral();
-
-
+bool isstring();
 
 int main(void){
    this = (code*)calloc(1, sizeof(code));
+   fp = fopen("demo2.ncl", "r");
 }
 
 void Prog(void){
@@ -60,7 +84,7 @@ void Prog(void){
       ERROR("No Begin statement ?");
    }
    this->currentrow++;
-   instru();
+   sinstru();
 }
 
 void sinstru(void){
@@ -139,12 +163,10 @@ bool ret(){
    return false;
 }
 
-
 bool listfunc(void){
    if (STRSAME(this->word[this->currentrow], "CAR")){
       this->currentrow++;
       islist();
-      lisp* current = lispfromstring
       this->currentrow++;
       return true;
    }
@@ -206,12 +228,34 @@ bool boolfunc(void){
    return false;
 }
 
+bool iofunc(void){
+   bool flag = false;
+   flag = set();
+   if (flag == true){
+      this->currentrow++;
+      return true;
+   }
+
+
+   flag = print();
+   if (flag == true){
+      this->currentrow++;
+      return true;
+   }
+
+   return false;
+}
+
 bool set(void){
    if (STRSAME(this->word[this->currentrow], "SET")){
       this->currentrow++;
-      assert(isvar(this->word[this->currentrow]));
+      if(!isvar(this->word[this->currentrow])){
+         ERROR("Set function miss var");
+      }
       this->currentrow++;
-      assert(islist(this->word[this->currentrow]));
+      if(!islist(this->word[this->currentrow])){
+         ERROR("Set function miss list");
+      }
    }
    return false;
 }
@@ -219,9 +263,78 @@ bool set(void){
 bool print(void){
    if (STRSAME(this->word[this->currentrow], "PRINT")){
       this->currentrow++;
-      assert(isvar(this->word[this->currentrow]));
+      if(islist()){
+         return true;
+      }else if(isstring()){
+         return true;
+      }else{
+         ERROR("Print function miss list or string element");
+      }
+   }
+   return false;
+}
+
+bool iffunc(void){
+   if (STRSAME(this->word[this->currentrow], "IF")){
       this->currentrow++;
-      assert(islist(this->word[this->currentrow]));
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ( in if function condition stage.");
+      }
+
+      this->currentrow++;
+      bool boolf = boolfunc();
+      if (!boolf){
+         ERROR("No boolfunctionb in if function condition stage.");
+      }
+
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ) in if function condition stage.");
+      }
+
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ( in if function first action stage.");
+      }
+
+      sinstru();
+
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ( in if function second action stage.");
+      }
+
+      sinstru();
+
+      return true;
+   }
+   return false;
+}
+bool loop(void){
+   if (STRSAME(this->word[this->currentrow], "WHILE")){
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ( in loop function condition stage.");
+      }
+
+      this->currentrow++;
+      bool boolf = boolfunc();
+      if (!boolf){
+         ERROR("No boolfunctionb in loop function condition stage.");
+      }
+
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ) in loop function condition stage.");
+      }
+
+      this->currentrow++;
+      if (!STRSAME(this->word[this->currentrow], "(")){
+         ERROR("No ( in loop function first action stage.");
+      }
+
+      sinstru();
+
       return true;
    }
    return false;
@@ -254,5 +367,13 @@ bool isvar(){
 }
 
 bool isliteral(){
-   return true;
+   int lastchar = (int)strlen(this->word[this->currentrow]) - 1;
+   return this->word[this->currentrow][0] == '\'' && this->word[this->currentrow][lastchar] == '\'';
 }
+
+bool isstring(){
+   int lastchar = (int)strlen(this->word[this->currentrow]) - 1;
+   return this->word[this->currentrow][0] == '\"' && this->word[this->currentrow][lastchar] == '\"';
+}
+
+
